aaf33009fdaec219d68c67563d8a2cc9fab96207
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-09-13 23:55:28 -0300

    Add auth pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <h1>Hello world</h1>
+
+      <UserButton afterSignOutUrl="/" />
+    </main>
+  );
 }
