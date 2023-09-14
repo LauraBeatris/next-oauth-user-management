d05ff0e27fa81395148a5eba98d33f06946a5fc5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-09-14 19:06:29 -0300

    Define server action to fetch calendar events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,56 @@
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, UserProfile } from "@clerk/nextjs";
+import { auth, clerkClient } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
 
 export default function Home() {
+  async function fetchContent() {
+    "use server";
+
+    const { userId } = auth();
+
+    const [OauthAccessToken] = await clerkClient.users.getUserOauthAccessToken(
+      userId ?? "",
+      "oauth_google"
+    );
+
+    const { token } = OauthAccessToken;
+
+    if (!token) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const calendarApi = google.calendar({
+      version: "v3",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const {
+      data: { items },
+    } = await calendarApi.calendarList.list();
+
+    /**
+     * Just accessing the first ID of the list for testing purposes
+     */
+    const calendarId = items?.[0].id;
+
+    if (calendarId) {
+      // TODO - Render events
+      const events = await calendarApi.events.list({
+        calendarId,
+      });
+    }
+  }
+
   return (
     <main>
-      <h1>Hello world</h1>
+      <form action={fetchContent}>
+        <button type="submit">Fetch content</button>
+      </form>
 
       <UserButton afterSignOutUrl="/" />
+
+      <UserProfile />
     </main>
   );
 }
